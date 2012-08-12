10c56b46a32dd0050e86208af46ac9ce950d64ed
Sterling <me@mynameissterling.com>
2012-08-12 18:51:45 -0400

    tweaks

--- app.js
+++ app.js
@@ -150,20 +150,17 @@ app.get('/redeem-beer', function(req, res) {
 
 
 
-var auth = 'Basic ' + new Buffer(configKey + ':' + secretKey).toString('base64');
-
-// auth is: 'Basic VGVzdDoxMjM='
-
+var auth = configKey + ':' + secretKey;
 var client = http.createClient(80, 'launch.alertrocket.com/api/push'); // to access this url i need to put basic auth.
 var header = {'Host': 'launch.alertrocket.com/api/push', 'Authorization': auth};
 var request = client.request('GET', '/', header);
 request.end();
 
 request.on('response', function (response) {
-  console.log('STATUS: ' + response.statusCode);
-  console.log('HEADERS: ' + JSON.stringify(response.headers));
-  response.setEncoding('utf8');
-  response.on('data', function (chunk) {
-    console.log('BODY: ' + chunk);
-  });
+	console.log('STATUS: ' + response.statusCode);
+	console.log('HEADERS: ' + JSON.stringify(response.headers));
+	response.setEncoding('utf8');
+	response.on('data', function (chunk) {
+		console.log('BODY: ' + chunk);
+	});
 });
