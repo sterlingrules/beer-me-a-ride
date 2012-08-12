83be977af8e8b892639a683293b86710675059f9
Sterling <me@mynameissterling.com>
2012-08-12 16:05:24 -0400

    update db connection

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.enable('jsonp callback');
 // --------------------------------------------------------------------------------------
 
 var mysql = require('mysql'),
-	database = 'beer-me-a-ride',
+	database = 'beer_me_a_ride',
 	user_table = 'users',
 	// client = mysql.createClient({ user: 'root', password: '' });
 	client = mysql.createClient({ user: 'sterlingrules', password: '@y&7~s45', host: 'mysql.mynameissterling.com', port: 3306 });
